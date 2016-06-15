89dd3fabed9903e268c6b7e899ee3eb4aa1f07c0
Ben Tyler <benjamin.tyler@booking.com>
2016-06-15 14:50:13 +0200

    carbonsearch: introduce expvar struct/helpers

--- util/util.go
+++ util/util.go
@@ -1,10 +1,60 @@
 package util
 
 import (
+	"expvar"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"strconv"
 )
+
+type Stats struct {
+	TagMessages *expvar.Int
+	TagsIndexed *expvar.Int
+
+	MetricMessages *expvar.Int
+	MetricsIndexed *expvar.Int
+
+	CustomMessages   *expvar.Int
+	FullIndexTags    *expvar.Int
+	FullIndexMetrics *expvar.Int
+
+	QueriesHandled     *expvar.Int
+	QueryTagsByService *expvar.Map
+
+	ServicesByIndex *expvar.Map
+
+	SplitIndexes *expvar.Map
+}
+
+func InitStats() *Stats {
+	return &Stats{
+		TagMessages: expvar.NewInt("TagMessages"),
+		TagsIndexed: expvar.NewInt("TagsIndexed"),
+
+		MetricMessages: expvar.NewInt("MetricMessages"),
+		MetricsIndexed: expvar.NewInt("MetricsIndexed"),
+
+		CustomMessages:   expvar.NewInt("CustomMessages"),
+		FullIndexTags:    expvar.NewInt("FullIndexTags"),
+		FullIndexMetrics: expvar.NewInt("FullIndexMetrics"),
+
+		QueriesHandled:     expvar.NewInt("QueriesHandled"),
+		QueryTagsByService: expvar.NewMap("QueryTagsByService"),
+
+		SplitIndexes: expvar.NewMap("SplitIndexes"),
+
+		ServicesByIndex: expvar.NewMap("ServicesByIndex"),
+	}
+}
+
+type ExpInt int
+
+func (i ExpInt) String() string { return strconv.Itoa(int(i)) }
+
+type ExpString string
+
+func (s ExpString) String() string { return string(s) }
 
 func ReadConfig(path string, dest interface{}) error {
 	bytes, err := ioutil.ReadFile(path)
