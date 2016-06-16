6b5f564085c9a505a02bd7618dc6ae717523e958
Ben Tyler <benjamin.tyler@booking.com>
2016-06-16 15:14:16 +0200

    carbonsearch: some basic split index benchmarks

--- index/split/split_test.go
+++ index/split/split_test.go
@@ -1,8 +1,12 @@
 package split
 
 import (
+	"math/rand"
 	"testing"
 )
+
+var seed int64 = 232342358902345
+var rnd *rand.Rand
 
 func TestQuery(t *testing.T) {
 	metricName := "server.hostname-1234"
@@ -29,4 +33,58 @@ func TestQuery(t *testing.T) {
 	if len(emptyResult) != 0 {
 		t.Errorf("found some results on a bogus query: %v", emptyResult)
 	}
+}
+
+func BenchmarkSmallsetQuery(b *testing.B) {
+	metricName := "server.hostname-1234"
+	host := "hostname-1234"
+	index := NewIndex("host")
+
+	index.AddMetrics(host, []string{metricName})
+	index.AddTags(host, []string{"server-state:live", "server-dc:lhr"})
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		index.Query([]string{"server-state:live"})
+	}
+}
+
+func BenchmarkLargesetQuery(b *testing.B) {
+	b.StopTimer()
+	rnd = rand.New(rand.NewSource(seed))
+	index := NewIndex("host")
+	hosts := rwords(100, 40)
+	queryTerms := []string{}
+	for _, host := range hosts {
+		index.AddMetrics(host, rwords(5000, 100))
+		tags := rwords(10, 30)
+		if rnd.Intn(15) == 1 {
+			queryTerms = append(queryTerms, tags[rnd.Int()%len(tags)])
+		}
+		index.AddTags(host, tags)
+	}
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		index.Query(queryTerms)
+	}
+}
+
+var alpha string = "abcdefghijklmnopqrstuvwxyz"
+
+func rwords(n int, wordMaxLen int) []string {
+	words := make([]string, n)
+	for i := 0; i < n; i++ {
+		l := rnd.Intn(wordMaxLen) + 1
+		word := make([]byte, l)
+		for j := 0; j < l; j++ {
+			word = append(word, rchr())
+		}
+		words = append(words, string(word))
+	}
+	return words
+}
+
+func rchr() byte {
+	return alpha[rnd.Int()%26]
 }
