2e733ad1dc12ce26f2487ac23917a6e4e595f240
Ben Tyler <benjamin.tyler@gmail.com>
2016-10-24 22:33:15 +0200

    tags: omit needless detail
    As far as I can see right now, we should never need the raw value for the
    'value' portion of a tag. We need a unique identifier for the 'key' so that we
    can know that we need to replace the hash which represents the previous
    iteration of this key. In doing so, we don't need the raw strings: a hash of
    'service-key' ought to do.
    
    This also shuffles around the tag parsing logic slightly.

--- tag/tag.go
+++ tag/tag.go
@@ -3,39 +3,47 @@ package tag
 import (
 	"fmt"
 	"strings"
+
+	"github.com/kanatohodets/carbonsearch/util"
 )
+
+type Key uint64
 
 // Parse separates a "service-key:value" tag into "service" and "key:value". If the tag is malformed an error is returned.
 func Parse(tag string) (string, string, error) {
+	serviceMark, _, err := validate(tag)
+	if err != nil {
+		return "", "", err
+	}
+
+	service := tag[0:serviceMark]
+	kv := tag[serviceMark+1:]
+	return service, kv, nil
+}
+
+// WriteKey returns a hash representing the write identity for this tag. The
+// write identity is the portion of the tag that's needed to update a value ("service-key" in "service-key:value")
+func WriteKey(tag string) (Key, error) {
+	_, kvMark, err := validate(tag)
+	if err != nil {
+		return 0, err
+	}
+	serviceKey := tag[0:kvMark]
+	return Key(util.HashStr64(serviceKey)), nil
+}
+
+func validate(tag string) (int, int, error) {
 	serviceDelimiter := strings.Index(tag, "-")
 	kvMarker := strings.Index(tag, ":")
 	hasDots := strings.Index(tag, ".")
 	if hasDots != -1 {
-		return "", "", fmt.Errorf("tag: %q is an invalid tag: it contains full-stop characters, which will confuse graphite clients", tag)
+		return 0, 0, fmt.Errorf("tag: %q is an invalid tag: it contains full-stop characters, which will confuse graphite clients", tag)
 	}
 
 	// has a '-', has a ':', and the ':' has at least one character between it and the '-'
 	if serviceDelimiter == -1 || kvMarker == -1 || kvMarker < serviceDelimiter+1 {
-		return "", "", fmt.Errorf("tag: %q is an invalid tag, should be: service-key:value", tag)
+		return 0, 0, fmt.Errorf("tag: %q is an invalid tag, should be: service-key:value", tag)
 	}
 
-	service := tag[0:serviceDelimiter]
-	kv := tag[serviceDelimiter+1:]
-	return service, kv, nil
-}
-
-// ParseKV separates a "service-key:value" tag into "service", "key", and "value". If the tag is malformed an error is returned.
-// This extra detail is only needed during writes, so that values can be updated.
-func ParseKV(tag string) (string, string, string, error) {
-	service, rawKV, err := Parse(tag)
-	if err != nil {
-		return "", "", "", err
-	}
-
-	// not 'strings.Split' because service-key:multi:part:value is acceptable
-	kvMarker := strings.Index(rawKV, ":")
-	key := rawKV[0:kvMarker]
-	value := rawKV[kvMarker+1:]
-
-	return service, key, value, nil
+	return serviceDelimiter, kvMarker, nil
 }
--- tag/tag_test.go
+++ tag/tag_test.go
@@ -2,6 +2,8 @@ package tag
 
 import (
 	"testing"
+
+	"github.com/kanatohodets/carbonsearch/util"
 )
 
 func TestParse(t *testing.T) {
@@ -48,35 +50,26 @@ func TestParse(t *testing.T) {
 	}
 }
 
-func TestParseKV(t *testing.T) {
-	validCases := map[string][]string{
-		"server-state:live":                          {"server", "state", "live"},
-		"discovery-status:live":                      {"discovery", "status", "live"},
-		"server-dc:lhr":                              {"server", "dc", "lhr"},
-		"lb-pool:www":                                {"lb", "pool", "www"},
-		"custom-favorites:btyler":                    {"custom", "favorites", "btyler"},
-		"server-interfaces:eth1:ip_address:10_1_2_3": {"server", "interfaces", "eth1:ip_address:10_1_2_3"},
+func TestWriteKey(t *testing.T) {
+	h := util.HashStr64
+	validCases := map[string]Key{
+		"server-state:live":                          Key(h("server-state")),
+		"discovery-status:live":                      Key(h("discovery-status")),
+		"server-dc:lhr":                              Key(h("server-dc")),
+		"lb-pool:www":                                Key(h("lb-pool")),
+		"custom-favorites:btyler":                    Key(h("custom-favorites")),
+		"server-interfaces:eth1:ip_address:10_1_2_3": Key(h("server-interfaces")),
 	}
 
 	for valid, expected := range validCases {
-		service, key, value, err := ParseKV(valid)
+		writeKey, err := WriteKey(valid)
 		if err != nil {
-			t.Errorf("tag test: %q failed to parse: %q", valid, err)
+			t.Errorf("tag test: %q failed to parse: %q.", valid, err)
 			continue
 		}
 
-		if service != expected[0] {
-			t.Errorf("tag test: %q ought to have service %q, but it has %q instead", valid, expected[0], service)
+		if writeKey != expected {
+			t.Errorf("tag test: the write key for tag %q ought to be %q, but instead it is %q", valid, expected, writeKey)
 		}
-
-		if key != expected[1] {
-			t.Errorf("tag test: %q ought to have key %q, but it has %q instead", valid, expected[1], key)
-		}
-
-		if value != expected[2] {
-			t.Errorf("tag test: %q ought to have value %q, but it has %q instead", valid, expected[2], value)
-		}
-
 	}
-	//NOTE(btyler) -- invalid cases are the same as for Parse. If that changes, add distinct invalid test cases for ParseKV.
 }
