5e8c2ef22e0269ce6f1751a032c7b1785110e4b5
Ben Tyler <benjamin.tyler@gmail.com>
2017-02-10 23:44:30 +0100

    document: switch to trigrams
    In order to work nicely with go-postings trigram-specific indexing

--- index/text/document/document.go
+++ index/text/document/document.go
@@ -2,15 +2,15 @@ package document
 
 import "fmt"
 
-const N = 4
+const N = 3
 
 func Tokenize(term string) ([]uint32, error) {
 	if len(term) < N {
 		return nil, fmt.Errorf("%s is too short to search on", term)
 	}
 
-	// len(term) - 3 for quadgrams
-	tokens := make([]uint32, 0, len(term)-3)
+	// len(term) - 2 for trigrams
+	tokens := make([]uint32, 0, len(term)-2)
 	for i := 0; i <= len(term)-N; i++ {
 		var chunk [N]byte
 		copy(chunk[:], term[i:i+N])
@@ -21,7 +21,7 @@ func Tokenize(term string) ([]uint32, error) {
 }
 
 func ngramize(s [N]byte) uint32 {
-	return uint32(s[0])<<24 | uint32(s[1])<<16 | uint32(s[2])<<8 | uint32(s[3])
+	return uint32(s[0])<<16 | uint32(s[1])<<8 | uint32(s[2])
 }
 
 func Validate(metrics []string) []string {
